1888da79fd4dd258775a79f7351a57bfa98613c3
MyCode07 <erem19992015@mail.ru>
2024-12-12 20:15:13 +0300

    add news project single news about pages

--- src/js/parts/filters.js
+++ src/js/parts/filters.js
@@ -3,32 +3,46 @@ const filterSortSpan = document.querySelector('.filter-sort__btn span');
 const filterSortSelect = document.querySelector('.filter-sort .select');
 const filterSortSelectElements = document.querySelectorAll('.filter-sort .select label');
 
-filterSortBtn.addEventListener('click', () => {
-    filterSortBtn.classList.toggle('_active');
-    filterSortSelect.classList.toggle('_active');
-});
+if (filterSortBtn) {
 
-filterSortSelectElements.forEach(element => {
-    element.addEventListener('click', (e) => {
-        const elementText = e.target.textContent;
-        filterSortSpan.textContent = elementText;
+
+    filterSortBtn.addEventListener('click', () => {
+        filterSortBtn.classList.toggle('_active');
+        filterSortSelect.classList.toggle('_active');
     });
-});
 
-document.addEventListener('click', function (e) {
-    let targetEl = e.target;
-    if (!targetEl.classList.contains('filter-sort__btn')) {
-        filterSortBtn.classList.remove('_active');
-        filterSortSelect.classList.remove('_active');
-    }
-})
-
-const filterSortCurrency = document.querySelectorAll('.filter-currency a');
-
-filterSortCurrency.forEach(element => {
-    element.addEventListener('click', (e) => {
-        element.classList.toggle('_active');
+    filterSortSelectElements.forEach(element => {
+        element.addEventListener('click', (e) => {
+            const elementText = e.target.textContent;
+            filterSortSpan.textContent = elementText;
+        });
     });
-});
+
+    document.addEventListener('click', function (e) {
+        let targetEl = e.target;
+        if (!targetEl.classList.contains('filter-sort__btn')) {
+            filterSortBtn.classList.remove('_active');
+            filterSortSelect.classList.remove('_active');
+        }
+    })
+
+    const filterSortCurrency = document.querySelectorAll('.filter-currency a');
+
+    filterSortCurrency.forEach(element => {
+        element.addEventListener('click', (e) => {
+            element.classList.toggle('_active');
+        });
+    });
+
+}
 
 
+const showTextBtn = document.querySelector('._show-text');
+const textBody = document.querySelector('._text-description__body');
+
+if (showTextBtn) {
+    showTextBtn.addEventListener('click', () => {
+        showTextBtn.classList.toggle('_active');
+        textBody.classList.toggle('_active');
+    });
+}
