37230b2c5dc9af3a913eeba3919fb83e76e3eb31
MyCode07 <erem19992015@mail.ru>
2024-12-13 11:44:08 +0300

    add basket error pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 // import { maskInputs } from "./static/inputmask.js";
 // import { accorden } from "./static/accordeon.js";
 // import { runTicker } from "./static/ticker.js";
-// import { replaceDomElements } from "./static/replace.js";
-// import { stickyHeader } from "./parts/header.js";
+import { replaceDomElements } from "./static/replace.js";
+import { stickyHeader } from "./parts/header.js";
 // import { playVideoAction } from "./parts/video.js";
 // import { range } from "./parts/range.js";
 
@@ -10,7 +10,7 @@
 // import "./parts/sliders.js";
 // import "./parts/popup.js";
 import "./parts/menu.js";
-// import "./parts/tabs.js";
+import "./parts/tabs.js";
 // import "./parts/service-image-hover.js";
 import "./parts/filters.js";
 // import "./parts/cart.js";
@@ -18,9 +18,9 @@ import "./parts/filters.js";
 
 // range();
 // accorden();
-// replaceDomElements();
+replaceDomElements();
 // runTicker();
-// stickyHeader(); 
+stickyHeader(); 
 // maskInputs('+7 (999) 999-99-99', '[name="phone"]')
 // playVideoAction();
 
